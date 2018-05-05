1dcb6660ab9e8270aeaeb7677e581dc5ee550a45
Unknown <branb11@mail.broward.edu>
2018-05-05 19:47:17 -0400

    Added Break/Lunch Button/Label

--- Digital_Clock/Digital_Clock/Form1.Designer.cs
+++ Digital_Clock/Digital_Clock/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.lblTime = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnBreak = new System.Windows.Forms.Button();
+            this.btnLunch = new System.Windows.Forms.Button();
+            this.timeBreak = new System.Windows.Forms.Label();
+            this.timeLunch = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTime
@@ -39,7 +43,7 @@
             this.lblTime.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTime.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblTime.Location = new System.Drawing.Point(36, 23);
+            this.lblTime.Location = new System.Drawing.Point(34, 9);
             this.lblTime.Name = "lblTime";
             this.lblTime.Size = new System.Drawing.Size(221, 58);
             this.lblTime.TabIndex = 0;
@@ -51,12 +55,54 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // btnBreak
+            // 
+            this.btnBreak.Location = new System.Drawing.Point(12, 97);
+            this.btnBreak.Name = "btnBreak";
+            this.btnBreak.Size = new System.Drawing.Size(75, 23);
+            this.btnBreak.TabIndex = 1;
+            this.btnBreak.Text = "Break";
+            this.btnBreak.UseVisualStyleBackColor = true;
+            // 
+            // btnLunch
+            // 
+            this.btnLunch.Location = new System.Drawing.Point(12, 144);
+            this.btnLunch.Name = "btnLunch";
+            this.btnLunch.Size = new System.Drawing.Size(75, 23);
+            this.btnLunch.TabIndex = 2;
+            this.btnLunch.Text = "Lunch";
+            this.btnLunch.UseVisualStyleBackColor = true;
+            // 
+            // timeBreak
+            // 
+            this.timeBreak.AutoSize = true;
+            this.timeBreak.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.timeBreak.Location = new System.Drawing.Point(144, 103);
+            this.timeBreak.Name = "timeBreak";
+            this.timeBreak.Size = new System.Drawing.Size(64, 17);
+            this.timeBreak.TabIndex = 3;
+            this.timeBreak.Text = "00:00:00";
+            // 
+            // timeLunch
+            // 
+            this.timeLunch.AutoSize = true;
+            this.timeLunch.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.timeLunch.Location = new System.Drawing.Point(144, 144);
+            this.timeLunch.Name = "timeLunch";
+            this.timeLunch.Size = new System.Drawing.Size(64, 17);
+            this.timeLunch.TabIndex = 4;
+            this.timeLunch.Text = "00:00:00";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(299, 106);
+            this.ClientSize = new System.Drawing.Size(299, 209);
+            this.Controls.Add(this.timeLunch);
+            this.Controls.Add(this.timeBreak);
+            this.Controls.Add(this.btnLunch);
+            this.Controls.Add(this.btnBreak);
             this.Controls.Add(this.lblTime);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -69,6 +115,10 @@
 
         private System.Windows.Forms.Label lblTime;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnBreak;
+        private System.Windows.Forms.Button btnLunch;
+        private System.Windows.Forms.Label timeBreak;
+        private System.Windows.Forms.Label timeLunch;
     }
 }
 
